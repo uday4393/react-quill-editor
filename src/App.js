33e6880429c50c11dfb0748e63b0d2f11f4995df
Uday M <uday.mydam@gmail.com>
2022-11-20 11:35:51 +0100

    Quill soln.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import 'react-quill/dist/quill.snow.css';
+import {useState} from 'react'
 import './App.css';
+import { useQuill } from "react-quilljs";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [value, setValue] =  useState("");
+  const { quill, quillRef } = useQuill();
+
+  const handleSave = () => {
+    const text = quill.getText();
+    setValue(text);
+    const cursorPosition = quill.getSelection().index;
+    if(cursorPosition){
+      quill.clipboard.dangerouslyPasteHTML(cursorPosition,
+        "<p><br/><br/></p>")
+      quill.insertText(cursorPosition, "&");
+      quill.setSelection(cursorPosition + 1);
+    }
+    }
+    return (
+      <div className="App">
+        <div style={{ width: 800, height: 500 }}>
+          <div style={{ width: 500, height: 300 }}>
+            <div>Saved State : {value}</div>
+            <div ref={quillRef} />
+          </div>  
+        </div>
+          <button onClick={handleSave}>Insert '&'</button>
+        </div>
+    )
 }
 
 export default App;
